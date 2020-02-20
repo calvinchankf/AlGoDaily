dfc0a8674ac6d16d5254014c42f7e73f7bcbe9bb
calvinchankf <chan9118kin@gmail.com>
2020-02-20 19:53:17 +0800

    day539 s2

--- leetcode/1029-two-city-scheduling/main.py
+++ leetcode/1029-two-city-scheduling/main.py
@@ -1,4 +1,5 @@
 from typing import List
+import sys
 
 """
     1st: sort
@@ -38,3 +39,35 @@ print(s.twoCitySchedCost(a))
 
 a = [[259, 770], [448, 54], [926, 667], [184, 139], [840, 118], [577, 469]]
 print(s.twoCitySchedCost(a))
+
+print('-----')
+
+"""
+    2nd: dynamic programming
+    - recursion to get all the combinations
+    - use a hashtable to avoid redundant calculation to optimize the speed from O(2^N) tp O(N^2)
+
+    Time    O(N^2)
+    Space   O(N^2)
+    128 ms, faster than 5.15%
+"""
+
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        self.cached = {}
+        return self.dfs(costs, 0, 0, 0)
+
+    def dfs(self, costs, count_a, count_b, index):
+        if (count_a, count_b, index) in self.cached:
+            return self.cached[(count_a, count_b, index)]
+        if count_a == count_b == len(costs) // 2:  # Valid division of people
+            return 0
+        if index == len(costs):
+            return sys.maxsize  # Invalid division of people
+        minCost = min(
+            costs[index][0] + self.dfs(costs, count_a + 1, count_b, index + 1),
+            costs[index][1] + self.dfs(costs, count_a, count_b + 1, index + 1)
+        )
+        self.cached[(count_a, count_b, index)] = minCost
+        return minCost
