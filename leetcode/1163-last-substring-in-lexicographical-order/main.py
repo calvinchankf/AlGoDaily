c8297dfe454a191d71dae3f911017e4e4c40a701
calvinchankf <chan9118kin@gmail.com>
2020-12-14 11:50:07 +0800

    review

--- leetcode/1163-last-substring-in-lexicographical-order/main.py
+++ leetcode/1163-last-substring-in-lexicographical-order/main.py
@@ -23,3 +23,53 @@ class Solution(object):
             if sfs > res:
                 res = sfs
         return res
+
+
+"""
+    2nd: 3 pointers
+    - learned from others
+    
+    idea:
+    i: start of left subtring
+    j: start of right substring
+    k: the shared length of substrings
+
+    e.g.1: when both string equal to each other, keep increasing k
+        i        j
+        zzzab....zzzab...
+    k=      ^        ^
+
+    e.g.2: when left substring is smaller, let left substring = right substring, so zzzaa becomes zzzab
+        i        j
+        zzzaa....zzzab...
+    k=      ^        ^
+                 ^new i
+
+    e.g.3: when right substring is smaller, start the right substring from j+k+1. Just like we start new right substring
+        i        j
+        zzzab....zzzaa...
+    k=      ^        ^
+                      ^new j
+
+    Time    O(N)
+    Space   O(1)
+    264 ms, faster than 64.49%
+"""
+
+
+class Solution:
+    def lastSubstring(self, s: str) -> str:
+        i = 0
+        j = i + 1
+        k = 0
+        while j + k < len(s):
+            if s[i+k] == s[j+k]:
+                k += 1
+            else:
+                if s[i+k] < s[j+k]:
+                    i = j
+                    j = i + 1
+                else:
+                    j = j + k + 1
+                k = 0
+        return s[i:]
