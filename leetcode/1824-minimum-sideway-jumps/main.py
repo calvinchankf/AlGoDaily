081041c86e740f2ca354abe8b05089962e50c57b
calvinchankf <chan9118kin@gmail.com>
2021-04-16 10:07:54 +0800

    push

--- leetcode/1824-minimum-sideway-jumps/main.py
+++ leetcode/1824-minimum-sideway-jumps/main.py
@@ -1,10 +1,10 @@
 """
-    1st: BFS
+    1st: BFS + hashset
     - for every point, there are 3 possibilities to move forward
     - dont step on the obstacles
     - dont step on the points you visited
 
-    Time    O(3N)
+    Time    O(3N) there are at most 3N cells
     Space   O(3N)
     8288 ms, faster than 5.42%
 """
@@ -32,10 +32,10 @@ class Solution:
 
 
 """
-    2nd: BFS
+    2nd: BFS + hashset
     - same as 1st but only put visitable points into the queue
 
-    Time    O(3N)
+    Time    O(3N) there are at most 3N cells
     Space   O(3N)
     7264 ms, faster than 10.09%
 """
@@ -61,3 +61,28 @@ class Solution:
             if obstacles[point]-1 != (lane + 2) % 3:
                 q.append((point, (lane + 2) % 3, jumps + 1))
         return 0
+
+
+"""
+    2nd: dynamic programming, bottom up
+    - there are 3 lanes only, and there are no stones on the first point so we can use [1,0,1] to represent the costs
+    - at every point, use the previous cost, and the cost from the neighbours for calculation
+
+    Time    O(3N)
+    Space   O(3) -> O(1)
+    3680 ms, faster than 63.44%
+"""
+
+
+class Solution:
+    def minSideJumps(self, obstacles: List[int]) -> int:
+        n = len(obstacles)
+        dp = [1, 0, 1]
+        for i in range(1, n):
+            laneIdx = obstacles[i] - 1
+            if laneIdx > -1:
+                dp[laneIdx] = 2**32
+            for j in range(3):
+                if j != laneIdx:
+                    dp[j] = min(dp[j], dp[(j+1) % 3] + 1, dp[(j+2) % 3] + 1)
+        return min(dp)
