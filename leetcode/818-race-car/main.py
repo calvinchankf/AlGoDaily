b056b4d9994776c15a66b6fe8fd0f880ad4cb296
calvinchankf <chan9118kin@gmail.com>
2021-07-01 12:05:07 +0100

    day1039 2nd

--- leetcode/818-race-car/main.py
+++ leetcode/818-race-car/main.py
@@ -2,7 +2,7 @@
 from collections import deque
 
 """
-    1st: optimized BFS + hashtable
+    1st: sub-optimal BFS + hashtable
     - instead of using an array, we use a deque. So the time complexity of dequeue() takes O(1)
 
     Time    O(2^N)
@@ -31,4 +31,32 @@ class Solution(object):
                 q.append((position, -1, steps + 1))
             else:
                 q.append((position, 1, steps + 1))
+        return -1
+
+
+"""
+    2nd: optimized BFS
+    - compute the position one-step further, and avoid appending illegitimate posistion into the queue
+
+    Time    O(2^N)
+    Space   O(N)
+    24 ms, faster than 98.15%
+"""
+
+
+class Solution(object):
+    def racecar(self, target):
+        q = deque()
+        q.append((0, 1, 0))
+        while len(q) > 0:
+            position, speed, steps = q.popleft()
+            if position+speed == target:
+                return steps+1
+            q.append((position+speed, speed*2, steps+1))
+            if speed > 0:
+                if position + speed > target:
+                    q.append((position, -1, steps+1))
+            else:
+                if position + speed < target:
+                    q.append((position, 1, steps+1))
         return -1
